2ca61a6fac532aa23a6adf4e2231120802fdc9be
Hello-Kitty010 <imoku1126@gmail.com>
2025-08-17 13:25:58 +0900

    Create pebble.lua
    When player touch the ground, player will die.

---
+++ obby_game/pebble.lua
@@ -0,0 +1,11 @@
+local pebble = script.Parent
+
+local function killPlayer(otherPart)
+	local partPlayer = otherPart.Parent
+	local humanoid = partPlayer:FindFirstChild("Humanoid")
+	if humanoid then
+		humanoid.Health = 0
+	end
+end
+
+pebble.Touched:Connect(killPlayer)
