5e1693ad0ccd69f530ecaef4710b8413bdd9f9ac
Hello-Kitty010 <imoku1126@gmail.com>
2025-08-17 13:40:06 +0900

    Create remained code of roblox
    create 2 files

---
+++ obby_game/finish_part_effect.lua
@@ -0,0 +1,23 @@
+local debounce = false
+local label = script.Parent
+local part = workspace:WaitForChild("CelebrationPart")
+
+local particle = part:FindFirstChildOfClass("ParticleEmitter")
+local sound = part:FindFirstChildOfClass("Sound")
+
+local function checkFinished(otherPart)
+	if not debounce then
+		debounce = true
+		local partParent = otherPart.Parent
+		local humanoid = partParent:FindFirstChild("Humanoid")
+		if humanoid then
+			label.Visible = true
+			particle.Enabled = true
+			sound:Play()
+		end
+		wait(33)
+		debounce = false
+	end
+end
+
+part.Touched:Connect(checkFinished)
